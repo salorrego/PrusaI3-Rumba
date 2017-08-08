d9760b592ca0559d3ca6179e44ac9a644506af03
Unknown <jcarlosbotero1@outlook.com>
2017-08-07 20:01:21 -0500

    Inverting Y

--- Marlin_1.1_Rumba/Marlin-1.1.x/Marlin-1.1.x/Marlin/Configuration.h
+++ Marlin_1.1_Rumba/Marlin-1.1.x/Marlin-1.1.x/Marlin/Configuration.h
@@ -432,7 +432,7 @@
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
-//#define USE_YMIN_PLUG
+#define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
 //#define USE_XMAX_PLUG
 #define USE_YMAX_PLUG
@@ -714,7 +714,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR true
+#define INVERT_Y_DIR false
 #define INVERT_Z_DIR false
 
 // Enable this option for Toshiba stepper drivers
@@ -737,7 +737,7 @@
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
 #define X_HOME_DIR -1
-#define Y_HOME_DIR 1
+#define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
 // @section machine
